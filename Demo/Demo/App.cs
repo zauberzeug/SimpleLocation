98f0ea4c7b08763ecc853e9fd1c503a5bfccdab3
Sascha Willam <sascha@perpetual-mobile.de>
2015-12-08 10:38:38 +0100

    Improved DemoApp.

--- Demo/Demo/App.cs
+++ Demo/Demo/App.cs
@@ -8,44 +8,74 @@ namespace Demo
     {
         public SimpleLocationManager SimpleLocationManager = new SimpleLocationManager();
 
-        string hello = "Hello, SimpleLocation!";
+        const string hello = "Hello, SimpleLocation!";
+        const string stopped = "Location updates stopped.";
+
+        Label helloLabel;
+        Label locationLabel;
+        Button startButton;
+        Button stopButton;
 
         public App()
         {
-            var label = new Label {
-                XAlign = TextAlignment.Center,
-                Text = hello,
-            };
-
-            var startButton = new Button {
-                Text = "Start location updates",
-                Command = new Command(o => SimpleLocationManager.StartLocationUpdates(
-                    LocationAccuracy.High, 1, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(1))),
-            };
-
-            var stopButton = new Button {
-                Text = "Stop location updates",
-                Command = new Command(o => SimpleLocationManager.StopLocationUpdates()),
-            };
+            InitViews();
 
             MainPage = new ContentPage {
                 Content = new StackLayout {
-                    VerticalOptions = LayoutOptions.Center,
+                    Padding = new Thickness(20, Device.OnPlatform(40, 20, 20), 20, 20),
+                    Spacing = 20,
                     Children = {
-                        label,
+                        helloLabel,
+                        locationLabel,
                         startButton,
                         stopButton,
                     },
                 },
             };
 
-            SimpleLocationManager.LocationUpdated += () => label.Text = "New location:\n" + SimpleLocationManager.LastLocation;
-            SimpleLocationManager.LocationUpdatesStopped += () => label.Text = hello;
+            SimpleLocationManager.LocationUpdated += delegate {
+                locationLabel.Text = string.Format(
+                    "New location:\n\n Latitude={0}\nLongitude={1}", 
+                    SimpleLocationManager.LastLocation.Latitude,
+                    SimpleLocationManager.LastLocation.Longitude);
+            };
+            SimpleLocationManager.LocationUpdatesStopped += () => locationLabel.Text = stopped;
         }
 
         protected override void OnSleep()
         {
             SimpleLocationManager.StopLocationUpdates();
+        }
+
+        void InitViews()
+        {
+            helloLabel = new Label {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                XAlign = TextAlignment.Center,
+                Text = hello,
+            };
+
+            locationLabel = new Label {
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                XAlign = TextAlignment.Center,
+            };
+
+            startButton = new Button {
+                VerticalOptions = LayoutOptions.End,
+                BackgroundColor = Color.Green,
+                TextColor = Device.OnPlatform(Color.Black, Color.White, Color.White),
+                Text = "Start location updates",
+                Command = new Command(o => SimpleLocationManager.StartLocationUpdates(
+                    LocationAccuracy.High, 1, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(1))),
+            };
+
+            stopButton = new Button {
+                VerticalOptions = LayoutOptions.End,
+                BackgroundColor = Color.Red,
+                TextColor = Device.OnPlatform(Color.Black, Color.White, Color.White),
+                Text = "Stop location updates",
+                Command = new Command(o => SimpleLocationManager.StopLocationUpdates()),
+            };
         }
 
     }

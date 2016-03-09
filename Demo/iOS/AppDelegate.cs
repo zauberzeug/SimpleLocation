4f8ffbe032b4c244ddf9a2686808d967a8878d92
Sascha Willam <sascha@perpetual-mobile.de>
2016-03-09 10:25:32 +0100

    DemoApp iOS: Log location updates to text file for tracking locations received from background.

--- Demo/iOS/AppDelegate.cs
+++ Demo/iOS/AppDelegate.cs
@@ -1,4 +1,6 @@
-﻿using Foundation;
+﻿using System;
+using System.IO;
+using Foundation;
 using PerpetualEngine.Location;
 using UIKit;
 using Xamarin.Forms;
@@ -9,15 +11,36 @@ namespace Demo.iOS
     [Register("AppDelegate")]
     public class AppDelegate : FormsApplicationDelegate
     {
+        string locationsLog = "";
+
         public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
         {
             Forms.Init();
 
             SimpleLocationManager.RequestAlwaysAuthorization = true;
 
-            LoadApplication(new App());
+            var app = new App();
+            app.SimpleLocationManager.LocationUpdated += delegate {
+                var locationDataString = string.Format(
+                                             "New location:\nLat={0}\nLng={1}", 
+                                             app.SimpleLocationManager.LastLocation.Latitude,
+                                             app.SimpleLocationManager.LastLocation.Longitude);
+                locationsLog += string.Format("{0}\n{1}\n\n", DateTime.Now, locationDataString);
+            };
+            app.SimpleLocationManager.LocationUpdatesStopped += delegate {
+                LogLocationUpdates();
+            };
+
+            LoadApplication(app);
 
             return base.FinishedLaunching(uiApplication, launchOptions);
+        }
+
+        void LogLocationUpdates()
+        {
+            var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var filename = Path.Combine(documents, "LocationsLog.txt");
+            File.WriteAllText(filename, locationsLog);
         }
     }
 }

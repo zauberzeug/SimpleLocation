958475b524a17981fc19d4f4d718cbd59496f637
Sascha Willam <sascha@perpetual-mobile.de>
2018-11-15 18:34:07 +0100

    Add dummy implementation for .NET to be compatible with test projects

---
+++ Net/SimpleLocationManager.Net.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PerpetualEngine.Location
+{
+    public partial class SimpleLocationManager
+    {
+        public void StartLocationUpdates(LocationAccuracy accuracy, double smallestDisplacementMeters,
+                                         TimeSpan? interval = null, TimeSpan? fastestInterval = null)
+        {
+        }
+
+        public void StopLocationUpdates()
+        {
+        }
+    }
+}
